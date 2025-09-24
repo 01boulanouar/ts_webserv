1265d3f8015eed2aa55b9d14eecf45c7e9827b8a
Mohamed Amine Boulanouar <moboulan@e2r6p3.1337.ma>
2025-09-24 17:04:33 +0100

    healthz

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import express from "express";
+import { Request, Response } from "express";
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.static("."));
+
+async function handlerReadiness(req: Request, res: Response): Promise<void> {
+    res.set({
+        "Content-Type": "text/plain",
+    });
+    res.send();
+
+}
+
+app.get('/healthz', handlerReadiness);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
