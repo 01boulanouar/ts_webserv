b26bef6f3f83b296890b73c538caf762858319d9
Mohamed Amine Boulanouar <moboulan@e2r6p3.1337.ma>
2025-09-24 18:29:50 +0100

    chirps

--- src/handler.ts
+++ src/handler.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { config } from "./config.js";
+import { error } from "console";
 
 export async function handlerReadiness(req: Request, res: Response): Promise<void> {
     res.set({
@@ -27,3 +28,40 @@ export async function handlerReset(req: Request, res: Response): Promise<void> {
     config.fileserverHits = 0;
     res.send("Reset the metrics");
 }
+
+export async function handerValidateChirp(req: Request, res: Response): Promise<void> {
+    type respBody = {
+        valid ?: Boolean,
+        error ?: string
+    }
+
+    let body = "";
+    let statusCode = 200;
+    let respBody: respBody = {
+            valid: true,
+        };
+    
+    res.header("Content-Type", "application/json");
+    
+    req.on("data", (chunck) => {
+        body += chunck;
+    });
+
+    req.on("end", () => {
+        try {
+            const chirp = JSON.parse(body);
+            if (chirp.body.length > 140)
+            {
+                statusCode = 400;
+                respBody = { error: "Chirp is too long" }
+            }
+        } catch (error) {
+            statusCode = 400;
+            respBody = { error: "Something went wrong" };
+        }
+        
+   
+        res.status(statusCode).send(JSON.stringify(respBody))
+    })
+}
+
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { middlewareLogResponses, middlewareMetricsInc } from "./middleware.js";
-import { handlerMetrics, handlerReadiness, handlerReset } from "./handler.js";
+import { handerValidateChirp, handlerMetrics, handlerReadiness, handlerReset } from "./handler.js";
 
 const app = express();
 const PORT = 8080;
@@ -9,9 +9,9 @@ app.use(middlewareLogResponses);
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
 app.get("/admin/metrics", handlerMetrics);
-app.get("/admin/reset", handlerReset);
+app.post("/admin/reset", handlerReset);
 app.get('/api/healthz', handlerReadiness);
-
+app.post('/api/validate_chirp', handerValidateChirp);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
