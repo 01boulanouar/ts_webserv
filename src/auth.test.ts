edcc83b4e912237470c7a48b627d43c1d81ebd5e
Mohamed Amine Boulanouar <moboulan@e2r6p3.1337.ma>
2025-09-26 10:26:28 +0100

    test

---
+++ src/auth.test.ts
@@ -0,0 +1,44 @@
+import { describe, it, expect, beforeAll } from "vitest";
+import { checkPasswordHash, hashPassword, makeJWT, Payload, validateJWT } from "./auth";
+
+describe("Password Hashing", () => {
+    const password1 = "correctPassword123!";
+    const password2 = "anotherPassword456!";
+    let hash1: string;
+
+    beforeAll(async () => {
+        hash1 = await hashPassword(password1);
+    });
+
+    it("should return true for correct password", async () => {
+        const result1 = await checkPasswordHash(password1, hash1);
+        const result2 = await checkPasswordHash(password2, hash1);
+        
+        expect(result1).toBe(true);
+        expect(result2).toBe(false);
+    });
+});
+
+
+
+describe("JWT Tokens", () => {
+    const userID1 = "user_1";
+    const userID2 = "user_2";
+    const secret = "secret_phrase";
+    let jwt1: string;
+    let jwt2: string;
+    let jwt3: string;
+
+    beforeAll(async () => {
+        jwt1 = makeJWT(userID1, 10000, secret);
+        jwt2 = makeJWT(userID2, 10000, secret);
+        jwt3 = makeJWT(userID2, 0, secret);
+    });
+
+    it("should return true for correct JWT", async () => {
+        const result1 = validateJWT(jwt1, secret);
+        expect(result1).toBe("user_1");
+        expect(() => validateJWT(jwt2, "not_the_secret")).toThrowError('invalid signature');
+        expect(() => validateJWT(jwt3, secret)).toThrowError('jwt expired');
+    });
+});
--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,6 @@
 import * as argon2 from "argon2";
+import * as jwt from "jsonwebtoken";
+import { UnauthorizedError } from "./error";
 
 export async function hashPassword(password: string): Promise<string> {
    return await argon2.hash(password);
@@ -6,4 +8,23 @@ export async function hashPassword(password: string): Promise<string> {
 
 export async function checkPasswordHash(password: string, hash: string): Promise<boolean> {
     return await argon2.verify(hash, password);
+}
+
+export type Payload = Pick<jwt.JwtPayload, "iss" | "sub" | "iat" | "exp">;
+
+export function makeJWT(userID: string, expiresIn: number, secret: string): string
+{
+
+    const payload: Payload = {
+        iss: "chirpy",
+        sub: userID,
+        iat: Math.floor(Date.now() / 1000),
+        exp:  Math.floor(Date.now() / 1000) + expiresIn,
+    }
+    return jwt.sign(payload, secret);
+}
+
+export function validateJWT(tokenString: string, secret: string): string {
+    const payload = jwt.verify(tokenString, secret)  as Payload;
+    return payload.sub!;
 }
